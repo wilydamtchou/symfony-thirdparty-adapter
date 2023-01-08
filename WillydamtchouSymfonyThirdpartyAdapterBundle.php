57e96951cfd07c906c844ff8cba0f320978c032b
afrikpay <willy.damtchou@afrikpay.com>
2023-01-08 16:34:45 +0100

    dev

--- WillydamtchouSymfonyThirdpartyAdapterBundle.php
+++ WillydamtchouSymfonyThirdpartyAdapterBundle.php
@@ -2,9 +2,9 @@
 
 namespace Willydamtchou\SymfonyThirdpartyAdapter;
 
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class WillydamtchouSymfonyThirdpartyAdapterBundle extends Bundle
 {
-
 }
---
+++ WillydamtchouSymfonyThirdpartyAdapterExtension.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Willydamtchou\SymfonyThirdpartyAdapter;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
+
+class WillydamtchouSymfonyThirdpartyAdapterExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $containerBuilder)
+    {
+        $loader = new YamlFileLoader(
+            $containerBuilder,
+            new FileLocator(__DIR__.'/config')
+        );
+        $loader->load('services.yaml');
+    }
+}
