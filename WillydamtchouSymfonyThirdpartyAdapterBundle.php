35ecc1c5ce006838a8532c30df2b7e45e4972693
afrikpay <willy.damtchou@afrikpay.com>
2023-01-08 16:10:25 +0100

    update dependency

---
+++ WillydamtchouSymfonyThirdpartyAdapterBundle.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Willydamtchou\SymfonyThirdpartyAdapter;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class WillydamtchouSymfonyThirdpartyAdapterBundle extends Bundle
+{
+
+}
